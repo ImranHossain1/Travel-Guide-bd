20551d63a2b30a3e4726ef62b8b73c761aea26fa
ImranHossain1 <imranhossai@gmail.com>
2021-12-16 01:35:37 +0600

    font awesome icons updated

--- src/component/District/District.js
+++ src/component/District/District.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrain } from '@fortawesome/free-solid-svg-icons';
 import './District.css';
+
+
 const District = (props) => {
     const {areaName, division,  established, flag, population ,travelCost}= props.district;
     
     const train = <FontAwesomeIcon icon={faTrain} className='icons'/>
-    // console.log(pr25ops);
+
+    // console.log(props);
     return (
         <div className='card border-0 py-3'>
             <div className="card district-container background" >
@@ -22,6 +25,11 @@ const District = (props) => {
                         className='btn-success border-1 rounded p-2'>
                             {train} Add Destination
                     </button>
+                    <div className='mt-2'>
+                    <i className="fab fa-facebook-square"></i>
+                    <i className="fab fa-instagram"></i>
+                    <i class="fab fa-twitter"></i>
+                    </div>
                
                 </div>
             </div>
