f0d21acfb81bdb0c499e4bda75e8bdcebdec1510
ImranHossain1 <imranhossai@gmail.com>
2021-12-15 20:37:57 +0600

    icon added

--- src/component/Districts/Districts.js
+++ src/component/Districts/Districts.js
@@ -13,8 +13,6 @@ const Districts = () => {
         });
     },[])
 
-
-
     const handleAddToCart = (district =>{
         const newTravelCart = [...travelCart, district];
         setTravelCart(newTravelCart);
@@ -25,7 +23,7 @@ const Districts = () => {
             <div className="row gx-5">
                 <div className="col-md-12 col-lg-9">
                     <div className="p-3 border bg-light">
-                        <h3 className='text-center my-5'>Districts available: {districts.length}</h3>
+                        {/* <h3 className='text-center my-5'>Districts available: {districts.length}</h3> */}
                         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                                 {
                                     districts.map(district => <District
--- src/component/TravelCart/TravelCart.js
+++ src/component/TravelCart/TravelCart.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import AddedTravelDestination from '../AddedTravelDestination/AddedTravelDestination';
 import './TravelCart.css';
 const TravelCart = (props) => {
     // console.log(props.travelCart)
     const {travelCart} = props;
+    console.log(travelCart);
     let total = 0;
     for ( const destination of travelCart){
         total = total + destination.travelCost;
     }
     return (
         <div>
-            <h4>Travel summary</h4>
-            <h4>Destination Added : <span>{travelCart.length}</span></h4>
-            <h4>Total cost: <span>{total}</span></h4>
+            <div className="mb-5 text-center gradient p-2 rounded-2 fw-bold ">
+                <h4 className="text-center">Travel summary</h4>
+                <h5 className="fst-italic font-color">Destination Added : <span>{travelCart.length}</span></h5>
+                <h4>Total cost: <span>{total}</span></h4>
+            </div>
+            <div>
+                {
+                    travelCart.map(district => <AddedTravelDestination
+                        key= {district.areaId}
+                        travelDestination = {district}
+                    ></AddedTravelDestination>)
+                }
+            </div>
         </div>
     );
 };
